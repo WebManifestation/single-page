51b259a891e889f7fb9ffaa0a91ce0c89f06f81f
Raphael Kottakal <raphael@webmanifestation.com>
2020-12-13 00:08:35 +0530

    complete all pages

--- src/index.js
+++ src/index.js
@@ -7,76 +7,106 @@ const video2Text = document.getElementById("video-text-2");
 const video3Text = document.getElementById("video-text-3");
 const menuBtn = document.getElementsByClassName("menu-btn");
 const menuOverlay = document.getElementById("menu-overlay");
+const videoEnd = document.getElementById("video-end");
 
-let isScrolling = false;
+let paraVideoDone = false;
+let firstVideoDone = false;
 
-// menuOverlay.addEventListener("click", handleMenuClose);
-
-video1.addEventListener("ended", onVideoOneEnd);
-changeVideoOnSmallScreens();
-
-video1Text.addEventListener("click", () => {
-  window.scrollTo({
-    top: window.innerHeight,
-    behavior: "smooth",
-  });
+window.scrollTo({
+  top: 0,
 });
+if (window.innerWidth > 600) {
+  // document.body.style.overflow = "hidden";
 
-video2Text.addEventListener("click", () => {
-  // console.log("hi");
-  window.scrollTo({
-    top: window.innerHeight * 2,
-    behavior: "smooth",
+  // menuOverlay.addEventListener("click", handleMenuClose);
+
+  video1.addEventListener("ended", onVideoOneEnd);
+  video2.addEventListener("ended", onVideoOneEnd);
+  video3.addEventListener("ended", onVideoOneEnd);
+
+  video1Text.addEventListener("click", () => {
+    const getRect = video2.getBoundingClientRect();
+    window.scrollTo({
+      top: window.pageYOffset + getRect.top,
+      behavior: "smooth",
+    });
   });
-});
 
-video3Text.addEventListener("click", () => {
-  window.scrollTo({
-    top: window.innerHeight * 3,
-    behavior: "smooth",
+  video2Text.addEventListener("click", () => {
+    const getRect = video3.getBoundingClientRect();
+    window.scrollTo({
+      top: window.pageYOffset + getRect.top,
+      behavior: "smooth",
+    });
   });
-});
 
-setVideoTextFadeInTime(video1, video1Text, 3);
-setVideoTextFadeInTime(video2, video2Text, 5);
-setVideoTextFadeInTime(video3, video3Text, 5);
+  video3Text.addEventListener("click", () => {
+    const getRect = videoEnd.getBoundingClientRect();
+    window.scrollTo({
+      top: window.pageYOffset + getRect.top,
+      behavior: "smooth",
+    });
+  });
+
+  setVideoTextFadeInTime(video1, video1Text, 3);
+  setVideoTextFadeInTime(video2, video2Text, 5);
+  setVideoTextFadeInTime(video3, video3Text, 5);
+
+  video1.addEventListener("canplay", () => {
+    window.scrollTo({
+      top: 0,
+    });
+    video1.play();
+    // playVideoWhenInView(video1);
+  });
+
+  video2.addEventListener("canplay", (e) => {
+    playVideoWhenInView(video2);
+  });
+  video3.addEventListener("canplay", () => {
+    playVideoWhenInView(video3);
+  });
+}
 
 window.addEventListener("scroll", handleScroll);
 
-video2.addEventListener("canplay", (e) => {
-  playVideoWhenInView(video2);
-});
-video3.addEventListener("canplay", () => {
-  playVideoWhenInView(video3);
-});
+paraVideo.addEventListener("ended", onVideoOneEnd);
+
 paraVideo.addEventListener("canplay", () => {
   playVideoWhenInView(paraVideo);
 });
 
 function handleScroll() {
-  // playVideoWhenInView(video1);
-  playVideoWhenInView(video2);
-  playVideoWhenInView(video3);
+  if (window.innerWidth > 600) {
+    playVideoWhenInView(video1);
+    playVideoWhenInView(video2);
+    playVideoWhenInView(video3);
+  }
   playVideoWhenInView(paraVideo);
 }
 
 function playVideoWhenInView(videoElem) {
+  const videoRect = videoElem.getBoundingClientRect();
+  const offset = 0.5;
   if (
-    videoElem.getBoundingClientRect().top <= window.innerHeight * 0.25 &&
-    videoElem.getBoundingClientRect().top >= -window.innerHeight * 0.25
+    videoRect.top <= window.innerHeight * offset &&
+    videoRect.top >= -window.innerHeight * offset
   ) {
-    videoElem.play();
+    if (videoElem.id === "para-video" && paraVideoDone) {
+      videoElem.pause();
+    } else if (videoElem.id === "video-1" && firstVideoDone) {
+      videoElem.pause();
+    } else {
+      videoElem.play();
+    }
   } else if (!videoElem.paused) {
     videoElem.pause();
   }
 }
 
 function setVideoTextFadeInTime(videoElem, textElem, time) {
-  // console.log(videoElem, textElem, time);
-
   videoElem.addEventListener("timeupdate", handleTimeUpdate);
   function handleTimeUpdate(e) {
-    // console.log(videoElem.id, videoElem.currentTime);
     if (videoElem.currentTime > time) {
       textElem.style.opacity = 1;
       videoElem.removeEventListener("timeupdate", handleTimeUpdate);
@@ -84,22 +114,35 @@ function setVideoTextFadeInTime(videoElem, textElem, time) {
   }
 }
 
-function onVideoOneEnd() {
+function onVideoOneEnd(e) {
+  if (e.target.id === "para-video") {
+    paraVideoDone = true;
+    return;
+  } else if (e.target.id === "video-1") {
+    firstVideoDone = true;
+  }
+  const targetRect = e.target.getBoundingClientRect();
+  // document.body.style.overflow = "initial";
+
   window.scrollTo({
-    top: window.innerHeight,
+    top: window.pageYOffset + targetRect.top + targetRect.height,
     behavior: "smooth",
   });
-}
-
-function changeVideoOnSmallScreens() {
-  if (window.innerWidth <= 480) {
-    video1.src = "videos/home-1-small.mp4";
-    video2.src = "videos/home-2-small.mp4";
-    video3.src = "videos/home-3-small.mp4";
-    paraVideo.src = "videos/home-4-small.mp4";
+  if (e.target.id == "video-3") {
+    document.body.style.overflow = "initial";
   }
 }
 
+changeVideoOnSmallScreens();
+
+function changeVideoOnSmallScreens() {
+  if (window.innerWidth <= 480) {
+    // video1.src = "videos/home-1-small.mp4";
+    // video2.src = "videos/home-2-small.mp4";
+    // video3.src = "videos/home-3-small.mp4";
+    paraVideo.src = "videos/home-4-small.mp4";
+  }
+}
 // setTimeout(() => {
 //   window.scrollTo({
 //     top: 0,
